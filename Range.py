3fa916cfec4ff7cb3d3ff56c2b1e9e152db6e00c
Drop-G <tedpetersiv@gmail.com>
2021-01-07 11:33:13 -0500

    permutation solution

--- Range.py
+++ Range.py
@@ -38,29 +38,27 @@ print(Solution().getRange(arr, 9))
 #         last = self.binarySearchIterative(arr, 0, len(arr) - 1, target, False)
 #         return [first, last]
 
-
-# def binarySearchIterative(self, arr, low, high, target, findFirst):
-#     while True:
-#         if high < low:
-#             return -1
-#         mid = low + (high - low) // 2
-#         if findFirst:
-#             if (mid == 0 or target > arr[mid - 1]) and arr[mid] == target:
-#                 return mid
-#             if target > arr[mid]:
-#                 low = mid + 1
+#     def binarySearchIterative(self, arr, low, high, target, findFirst):
+#         while True:
+#             if high < low:
+#                 return -1
+#             mid = low + (high - low) // 2
+#             if findFirst:
+#                 if (mid == 0 or target > arr[mid - 1]) and arr[mid] == target:
+#                     return mid
+#                 if target > arr[mid]:
+#                     low = mid + 1
+#                 else:
+#                     high = mid - 1
 #             else:
-#                 high = mid - 1
-#         else:
-#             if (mid == len(arr)-1 or target < arr[mid + 1]) and arr[mid] == target:
-#                 return mid
-#             elif target < arr[mid]:
-#                 high = mid - 1
-#             else:
-#                 low = mid + 1
+#                 if (mid == len(arr)-1 or target < arr[mid + 1]) and arr[mid] == target:
+#                     return mid
+#                 elif target < arr[mid]:
+#                     high = mid - 1
+#                 else:
+#                     low = mid + 1
 
-
-# arr = [1, 3, 3, 5, 7, 8, 9, 9, 9, 15]
-# x = 9
-# print(Solution().getRange(arr, 9))
-# # [6, 8]
+#     arr = [1, 3, 3, 5, 7, 8, 9, 9, 9, 15]
+#     x = 9
+#     print(Solution().getRange(arr, 9))
+#     # [6, 8]
