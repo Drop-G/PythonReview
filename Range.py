48e20ebc17678af719a811d23dd127679396dec9
Drop-G <tedpetersiv@gmail.com>
2021-01-07 11:43:17 -0500

    iterative solution to getRange debugged

--- Range.py
+++ Range.py
@@ -31,34 +31,32 @@ print(Solution().getRange(arr, 9))
 
 # ---------------------------------------------------------------------------------------------------
 
+def getRange(self, arr, target):
+    first = self.binarySearchIterative(arr, 0, len(arr) - 1, target, True)
+    last = self.binarySearchIterative(arr, 0, len(arr) - 1, target, False)
+    return [first, last]
 
-# class Solution:
-#     def getRange(self, arr, target):
-#         first = self.binarySearchIterative(arr, 0, len(arr) - 1, target, True)
-#         last = self.binarySearchIterative(arr, 0, len(arr) - 1, target, False)
-#         return [first, last]
+def binarySearchIterative(self, arr, low, high, target, findFirst):
+    while True:
+        if high < low:
+            return -1
+        mid = low + (high - low) // 2
+        if findFirst:
+            if (mid == 0 or target > arr[mid - 1]) and arr[mid] == target:
+                return mid
+            if target > arr[mid]:
+                low = mid + 1
+            else:
+                high = mid - 1
+        else:
+            if (mid == len(arr)-1 or target < arr[mid + 1]) and arr[mid] == target:
+                return mid
+            elif target < arr[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
 
-#     def binarySearchIterative(self, arr, low, high, target, findFirst):
-#         while True:
-#             if high < low:
-#                 return -1
-#             mid = low + (high - low) // 2
-#             if findFirst:
-#                 if (mid == 0 or target > arr[mid - 1]) and arr[mid] == target:
-#                     return mid
-#                 if target > arr[mid]:
-#                     low = mid + 1
-#                 else:
-#                     high = mid - 1
-#             else:
-#                 if (mid == len(arr)-1 or target < arr[mid + 1]) and arr[mid] == target:
-#                     return mid
-#                 elif target < arr[mid]:
-#                     high = mid - 1
-#                 else:
-#                     low = mid + 1
-
-#     arr = [1, 3, 3, 5, 7, 8, 9, 9, 9, 15]
-#     x = 9
-#     print(Solution().getRange(arr, 9))
-#     # [6, 8]
+arr = [1, 3, 3, 5, 7, 8, 9, 9, 9, 9, 15]
+x = 9
+print(Solution().getRange(arr, 9))
+# [6, 8]
